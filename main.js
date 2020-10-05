f829de8a5d72e618cc160a3371fc20167a6cc443
Sven Buettner <sven.buettner1@gmail.com>
2020-10-05 14:31:47 +0900

    Finish palindrome tester site

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 let Phrase = require("mhartl-palindrome")
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string)
+function palindromeTester(event) {
+  event.preventDefault()
+  let phrase = new Phrase(event.target.phrase.value)
+  let palindromeResult = document.querySelector("#palindromeResult")
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`)
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let tester = document.querySelector("#palindromeTester")
+  tester.addEventListener("submit", palindromeTester)
+})
